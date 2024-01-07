12edbbe5d2b1390a2a8d0c81275585f7a7bbeb22
z004hsuy <anton.hofmeier@siemens.com>
2024-01-07 16:54:11 +0100

    init

---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+import time
+import threading
+from conv_service import convert_file, delayed_delete
+from flask import Flask, render_template, request, send_file
+
+app = Flask(__name__)
+
+temp_dir = 'temp'
+os.makedirs(temp_dir, exist_ok=True)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    uploaded_file = request.files['file']
+    selected_format = request.form.get('format')
+    
+    if uploaded_file and uploaded_file.filename != '':
+        temp_path = os.path.join(temp_dir, uploaded_file.filename)
+        uploaded_file.save(temp_path)
+
+        output_filename = os.path.splitext(uploaded_file.filename)[0] + '.' + selected_format
+        output_path = os.path.join(temp_dir, output_filename)
+
+        try:
+            convert_file(selected_format, temp_path, output_path)
+        except Exception as e:
+            return "An error occurred during file processing.", 500
+
+        response = send_file(output_path, as_attachment=True)
+        delayed_delete(temp_path)
+        delayed_delete(output_path)
+        return response
+
+    return 'No file uploaded', 400
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
---
+++ conv_service.py
@@ -0,0 +1,27 @@
+import os
+import ffmpeg
+import threading
+import time
+
+def convert_file(output_format, input_path, output_path):
+    print(f"Input Path: {input_path}, Output Path: {output_path}, Output Format: {output_format}")
+    try:
+        if output_format == 'mp4':
+            ffmpeg.input(input_path).output(output_path, vcodec='libx264', pix_fmt='yuv420p').run(overwrite_output=True)
+        elif output_format == 'gif':
+            ffmpeg.input(input_path).output(output_path).run()
+        elif output_format == 'png' or output_format == 'jpg' or output_format == 'jpeg':
+            ffmpeg.input(input_path).output(output_path, vframes=1).run()
+    except ffmpeg.Error as e:
+        print(e.stderr)
+        raise
+
+def delayed_delete(file_path, delay=10):
+    def task():
+        try:
+            time.sleep(delay)
+            os.remove(file_path)
+        except Exception as e:
+            print(f"Error deleting file {file_path}: {e}")
+
+    threading.Thread(target=task).start()
